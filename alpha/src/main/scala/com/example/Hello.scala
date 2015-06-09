cde23561475cf39d998c2dd5aedbbd946069efd2
Marcos Savoury <marcossavoury@hotmail.com>
2015-06-09 08:12:52 -0400

    basic example

--- alpha/src/main/scala/com/example/Hello.scala
+++ alpha/src/main/scala/com/example/Hello.scala
@@ -1,27 +1,33 @@
 package com.example
 
-import com.example.actors.HelloWorldActor
+import akka.actor.{Actor, Props, ActorSystem}
 
+class TestActor extends Actor {
+  def receive = {
+    case "Good Morning" => println("Good Morning Sire!")
+    case "Good Night" => println("Good Night to you as well")
+  }
+}
 /**
- * This is actually just a small wrapper around the generic launcher
- * class akka.Main, which expects only one argument: the class name of
- * the application?s main actor. This main method will then create the
- * infrastructure needed for running the actors, start the given main
- * actor and arrange for the whole application to shut down once the main
- * actor terminates.
- *
- * Thus you could also run the application with a
- * command similar to the following:
- * java -classpath  akka.Main com.example.actors.HelloWorldActor
- *
- * @author alias
  */
-object HelloSimpleMain {
+object Main {
+
+
+    val system = ActorSystem("BadShakespeare")
+    val actor = system.actorOf(Props[TestActor], "Shake")
+
+
+  def send(msg:String ): Unit = {
+    println(s"Me: $msg")
+    actor ! msg
+    Thread.sleep(100)
+  }
 
   def main(args: Array[String]): Unit = {
-    val initialActor = classOf[HelloWorldActor].getName
-
-    akka.Main.main(Array(initialActor))
+    send("Good Morning")
+    send("Good Night")
+    send("Unknown")
+    system.shutdown()
   }
 
 }
